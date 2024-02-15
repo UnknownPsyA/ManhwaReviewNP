b2a39986ac6346cfb6d4d5133442f7b5f43d7fbc
UnknownPsyA <124895386+UnknownPsyA@users.noreply.github.com>
2024-02-15 21:08:35 +0545

    Add files via upload
    Signed-off-by: UnknownPsyA <124895386+UnknownPsyA@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manhwa Review NP</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <?php include("includes/_header.php");?>
+    <div class="mainbody">
+    <div class="post-body">
+        <div class="swiper">
+
+        </div>
+        <div class="popular-today">
+
+        </div>
+        <div class="posts">
+            
+        </div>
+    </div>
+    <div class="popular">
+
+    </div>
+    </div>
+    
+</body>
+</html>
